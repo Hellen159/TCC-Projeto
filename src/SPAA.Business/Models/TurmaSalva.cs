d07e5ed739811cc8cd95128d7ba92f136773c275
Hellen159 <202016480@aluno.unb.br>
2025-06-09 22:32:43 -0300

    Teste de spaa.app e implementando tabela de turma salva.

---
+++ src/SPAA.Business/Models/TurmaSalva.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPAA.Business.Models
+{
+    public class TurmaSalva
+    {
+        public int CodigoTurmaSalva { get; set; }
+        public int CodigoUnicoTurma{ get; set; }
+        public string Matricula { get; set; }
+        public string CodigoDisciplina { get; set; }
+    }
+}
