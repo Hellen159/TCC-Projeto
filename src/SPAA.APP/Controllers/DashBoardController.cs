631cf24fbc6bd0e3e9e038b06e9be7950231ec50
Hellen159 <202016480@aluno.unb.br>
2025-06-23 18:57:45 -0300

    Adicionando autorização para as rotas.

--- src/SPAA.APP/Controllers/DashBoardController.cs
+++ src/SPAA.APP/Controllers/DashBoardController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SPAA.App.ViewModels;
 
 namespace SPAA.App.Controllers
 {
+    [Authorize]
     public class DashBoardController : Controller
     {
         public IActionResult Index()
